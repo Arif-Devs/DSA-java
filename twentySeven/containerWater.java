50b594c5b8535720a777114b0326e9a88f470cbb
Arifur Rahman <arifur.sew@gmail.com>
2023-11-09 16:56:53 +0600

    brute force & 2 pointer approach done

--- twentySeven/containerWater.java
+++ twentySeven/containerWater.java
@@ -1,18 +1,46 @@
 package twentySeven;
+
 import java.util.ArrayList;
-public class containerWater {
-    public static int waterStore(ArrayList<Integer> height) {
-        int maxWater = 0;
-        for (int i = 0; i < height.size(); i++) {
-            for (int j = i + 1; j < height.size(); j++) {
-                int maxHeight = Math.min(height.get(i), height.get(j));
-                int width = j - i;
-                int currentWater = maxHeight * width;
-                maxWater = Math.max(maxWater, currentWater);
-            }
+//Brute Force approach
+
+ public class containerWater {
+//     public static int waterStore(ArrayList<Integer> height) {
+//         int maxWater = 0;
+//         for (int i = 0; i < height.size(); i++) {
+//             for (int j = i + 1; j < height.size(); j++) {
+//                 int maxHeight = Math.min(height.get(i), height.get(j));
+//                 int width = j - i;
+//                 int currentWater = maxHeight * width;
+//                 maxWater = Math.max(maxWater, currentWater);
+//             }
+//         }
+//         return maxWater;
+//     }
+
+
+//2 pointer approach
+
+public static int waterCount(ArrayList<Integer> height) {
+    int maxWater = 0;
+    int leftPointer = 0;
+    int rightPointer = height.size() - 1;
+
+    while (leftPointer < rightPointer) {
+        int ht = Math.min(height.get(leftPointer), height.get(rightPointer));
+        int width = rightPointer - leftPointer;
+        int currentWater = ht * width;
+        maxWater = Math.max(maxWater, currentWater);
+
+        if (height.get(leftPointer) < height.get(rightPointer)) {
+            leftPointer++;
+        } else {
+            rightPointer--;
         }
-        return maxWater;
     }
+    return maxWater;
+}
+    
+
 
     public static void main(String args[]) {
         ArrayList<Integer> height = new ArrayList<>();
@@ -26,6 +54,6 @@ public class containerWater {
         height.add(8);
         height.add(3);
         height.add(7);
-        System.out.println(waterStore(height));
+        System.out.println(waterCount(height));
     }
 }
